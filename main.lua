71ad956811a793fc5551d237526f4f9de2f8261d
ffyt22487-gif <ffyt22487@gmail.com>
2026-07-09 23:50:20 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,12 @@
 print("Hello SeiHub!")
+local Scripts = {
+    [79546208627805] = "https://rawcdn.githack.com/ffyt22487-gif/Main-Script/b904d7f19ce1a6ccb4647b16a1383453b6b50aea/99_Nights_in_the_Forest.txt",
+    [126371807511901] = "https://rawcdn.githack.com/ffyt22487-gif/Main-Script/b904d7f19ce1a6ccb4647b16a1383453b6b50aea/99_Nights_in_the_Forest.txt",
+    [126509999114328] = "https://rawcdn.githack.com/ffyt22487-gif/Main-Script/b904d7f19ce1a6ccb4647b16a1383453b6b50aea/99_Nights_in_the_Forest.txt",
+    [113533413485661] = "https://raw.githubusercontent.com/ffyt22487-gif/Main-Script/main/Zombie_Tycoon.txt"
+}
+
+local URL = Scripts[game.PlaceId]
+if URL then
+    loadstring(game:HttpGet(URL))()
+end
