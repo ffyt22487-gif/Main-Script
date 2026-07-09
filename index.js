93f0d6e9a9e27c09c26998a7789161027578ac8a
ffyt22487-gif <ffyt22487@gmail.com>
2026-07-09 23:01:57 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,21 +3,29 @@ const fs = require("fs");
 
 const app = express();
 
+const API_KEY = "FreeFire598";
+
 app.get("/", (req, res) => {
     res.send("SeiHub API Online");
 });
 
 app.get("/script", (req, res) => {
+    const key = req.query.key;
+
+    if (key !== API_KEY) {
+        return res.status(403).send("Forbidden");
+    }
+
     try {
         const script = fs.readFileSync("main.lua", "utf8");
         res.setHeader("Content-Type", "text/plain");
         res.send(script);
-    } catch (err) {
+    } catch {
         res.status(500).send("Script not found.");
     }
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Running on ${PORT}`);
 });
